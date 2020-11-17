e65a6a31f92ff012db6df09eef75050363a10f23
iiConick <connor.mcelroy@ontariotechu.net>
2020-11-16 19:38:04 -0500

    Update MainEntities.h

--- Assignment-2-Essential-Maths-Level-Creation/SpriteLib3.0-v2.0/MainEntities.h
+++ Assignment-2-Essential-Maths-Level-Creation/SpriteLib3.0-v2.0/MainEntities.h
@@ -7,14 +7,16 @@ public:
 	//Getters
 	static int MainCamera();
 	static int MainPlayer();
+	static int GetFocus();
 
 	//Setters
 	static void MainCamera(int main);
 	static void MainPlayer(int main);
-
+	static void SetFocus(int main);
 private:
 	static int m_mainCamera;
 	static int m_mainPlayer;
+	static int m_focus;
 };
 
 #endif // !__MAINENTITIES_H__
